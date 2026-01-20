8802eaab061890c90d96d8ad3cf244c7b468978d
gegiske <gegiske@stud.ntnu.no>
2026-01-20 14:26:32 +0100

    setup task4 java class

---
+++ exercise1/core/src/main/java/com/example/exercise1/Task4.java
@@ -0,0 +1,47 @@
+package com.example.exercise1;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Task4 extends ApplicationAdapter {
+    public static final float WORLD_WIDTH = 800;
+    public static final float WORLD_HEIGHT = 480;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+
+
+
+        camera = new OrthographicCamera(); // Camera with no perspective for 2D
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera); // Handles the scaling on different devices
+        camera.position.set(WORLD_WIDTH / 2f, WORLD_HEIGHT / 2f, 0); // 2f puts the camera in center using float
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined); // sets origo to be bottom left of world not screen
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // This keeps your game scaling correctly if the window size changes
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+}
--- exercise1/lwjgl3/src/main/java/com/example/exercise1/lwjgl3/Lwjgl3Launcher.java
+++ exercise1/lwjgl3/src/main/java/com/example/exercise1/lwjgl3/Lwjgl3Launcher.java
@@ -3,6 +3,7 @@ package com.example.exercise1.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.example.exercise1.MyGame;
+import com.example.exercise1.Task4;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,7 +13,7 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new MyGame(), getDefaultConfiguration());
+        return new Lwjgl3Application(new Task4(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
